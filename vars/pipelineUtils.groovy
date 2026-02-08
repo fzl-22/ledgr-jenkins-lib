7aac154aaa44e0a87be25fcd7f9f5fa9162ceb2c
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2026-02-08 08:39:43 +0700

    chore: remove branch name on metadata

--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -32,7 +32,6 @@ def getMetadata(String branchName) {
   def isDeployable = getIsDeployable(env)
 
   return [
-    'BRANCH_NAME': branchName,
     'PROJECT_ENV': env,
     'PROJECT_NAMESPACE': namespace,
     'IS_DEPLOYABLE': isDeployable,
