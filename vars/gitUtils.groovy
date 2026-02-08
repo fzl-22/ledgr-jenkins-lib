e4a989355fb0ddeda17be3a638fc3794ab872ae7
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2026-02-08 09:04:15 +0700

    chore: add git get metadata

--- vars/gitUtils.groovy
+++ vars/gitUtils.groovy
@@ -1,15 +1,15 @@
-def getBranch() {
-  def branch = ""
+def getBranchName() {
+  def branchName = ""
 
   if (env.BRANCH_NAME) {
-    branch = env.BRANCH_NAME
+    branchName = env.BRANCH_NAME
   } else if (env.GIT_BRANCH) {
-    branch = env.GIT_BRANCH
+    branchName = env.GIT_BRANCH
   } else {
     try {
-      branch = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
+      branchName = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
     } catch (Exception e) {
-      echo "Warning: Could not determine git branch via command. Details: ${e.getMessage()}"
+      echo "Warning: Could not determine git branch name via command. Details: ${e.getMessage()}"
       return "unknown"
     }
   }
@@ -17,11 +17,32 @@ def getBranch() {
   return branch.replace('origin/', '')
 }
 
-def getShortCommit() {
+def getShortCommitID() {
   try {
     return sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
   } catch (Exception e) {
-    echo "⚠️ Warning: Could not determine git commit. Details: ${e.getMessage()}"
+    echo "Warning: Could not determine short git commit id. Details: ${e.getMessage()}"
     return "unknown"
   }
+}
+
+def getFullCommitID() {
+  try {
+    return sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+  } catch (Exception e) {
+    echo "Warning: Could not determine full git commit id. Details: ${e.getMessage()}"
+    return "unknown"
+  }
+}
+
+def getMetadata() {
+  def branchName = getBranchName()
+  def shortCommitID = getShortCommitID()
+  def fullCommitID = getFullCommitID()
+
+  return [
+    'GIT_BRANCH_NAME': branchName,
+    'GIT_SHORT_COMMIT_ID': shortCommitID,
+    'GIT_FULL_COMMIT_ID': fullCommitID,
+  ]
 }
